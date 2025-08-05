5a3f4a7f51b27e11cc82b395c9f563d525c82ca0
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-06 01:33:19 +0200

    Custom Creative Tab + 1.21.1

--- src/main/java/net/termineter/tutorialmod/TutorialMod.java
+++ src/main/java/net/termineter/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.termineter.tutorialmod;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.termineter.tutorialmod.blocks.ModBlocks;
+import net.termineter.tutorialmod.item.ModCreativeModeTabs;
 import net.termineter.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -40,6 +41,8 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModCreativeModeTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
@@ -74,7 +77,7 @@ public class TutorialMod {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = TutorialMod.MOD_ID, value = Dist.CLIENT)
     static class ClientModEvents {
         @SubscribeEvent
         static void onClientSetup(FMLClientSetupEvent event) {
