73c4dd4f16a24da616385900548f9ca6aae27aa5
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-06 16:09:01 +0200

    Custom tags

--- src/main/java/net/termineter/tutorialmod/blocks/custom/MagicBlock.java
+++ src/main/java/net/termineter/tutorialmod/blocks/custom/MagicBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.termineter.tutorialmod.item.ModItems;
+import net.termineter.tutorialmod.util.ModTags;
 
 import java.util.List;
 
@@ -36,7 +37,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()) {
+            if(isValidItem(itemEntity.getItem())) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
 
@@ -46,6 +47,10 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEM);
     }
 
     @Override
