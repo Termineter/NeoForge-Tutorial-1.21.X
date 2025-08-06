51253e4b1a358feb823c0c36e9c553264bacdb28
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-06 15:07:14 +0200

    Custom advanced block

--- src/main/java/net/termineter/tutorialmod/blocks/ModBlocks.java
+++ src/main/java/net/termineter/tutorialmod/blocks/ModBlocks.java
@@ -11,6 +11,7 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.termineter.tutorialmod.TutorialMod;
+import net.termineter.tutorialmod.blocks.custom.MagicBlock;
 import net.termineter.tutorialmod.item.ModItems;
 
 import java.util.function.Supplier;
@@ -30,6 +31,10 @@ public class ModBlocks {
     public static final DeferredBlock<Block> BISMUTH_DEEPSLATE_ORE = registerBlock("bismuth_deepslate_ore",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
+    public static final DeferredBlock<Block> MAGIC_BLOCK = registerBlock("magic_block",
+            () -> new MagicBlock(BlockBehaviour.Properties.of()
+                    .strength(2f).requiresCorrectToolForDrops()));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
