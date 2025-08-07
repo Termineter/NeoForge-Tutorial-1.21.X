128f19f1184b7fd3abee459fa30ab186a5ed63ed
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-07 21:50:35 +0200

    Data Components fix

--- src/main/java/net/termineter/tutorialmod/component/ModDataComponents.java
+++ src/main/java/net/termineter/tutorialmod/component/ModDataComponents.java
@@ -2,6 +2,7 @@ package net.termineter.tutorialmod.component;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -10,7 +11,7 @@ import net.termineter.tutorialmod.TutorialMod;
 import java.util.function.UnaryOperator;
 
 public class ModDataComponents {
-    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(TutorialMod.MOD_ID);
+    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE,TutorialMod.MOD_ID);
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> COORDINATES = register("coordinates",
             builder -> builder.persistent(BlockPos.CODEC));
