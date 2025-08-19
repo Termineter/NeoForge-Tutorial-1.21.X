7550b7c3b436d0eb3ff81de8bc12c5c8963f0552
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-20 00:27:42 +0200

    world gen setup

--- src/main/java/net/termineter/tutorialmod/datagen/ModDatapackProvider.java
+++ src/main/java/net/termineter/tutorialmod/datagen/ModDatapackProvider.java
@@ -5,11 +5,15 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.termineter.tutorialmod.TutorialMod;
 import net.termineter.tutorialmod.enchantment.ModEnchantmentEffects;
 import net.termineter.tutorialmod.enchantment.ModEnchantments;
 import net.termineter.tutorialmod.trim.ModTrimMaterials;
 import net.termineter.tutorialmod.trim.ModTrimPatterns;
+import net.termineter.tutorialmod.worldgen.ModBiomeModifiers;
+import net.termineter.tutorialmod.worldgen.ModConfiguredFeatures;
+import net.termineter.tutorialmod.worldgen.ModPlacedFeatures;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -19,7 +23,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
