32af70e2ca386d66bb122f0f80b855e7841c0fab
Kevin <kevin.watzke001@stud.fh-dortmund.de>
2025-08-21 23:40:16 +0200

    booster fov change

--- src/main/java/net/termineter/tutorialmod/events/ModClientEvents.java
+++ src/main/java/net/termineter/tutorialmod/events/ModClientEvents.java
@@ -24,6 +24,19 @@ public class ModClientEvents {
             fovModifier *= 1f - deltaTicks * 0.15f;
             event.setNewFovModifier(fovModifier);
         }
+
+        if(event.getPlayer().isUsingItem() && event.getPlayer().getUseItem().getItem() == ModItems.BOOSTER.get()) {
+            float fovModifier = 1f;
+            int ticksUsingItem = event.getPlayer().getTicksUsingItem();
+            float deltaTicks = (float) ticksUsingItem / 60f;
+            if (deltaTicks > 1f) {
+                deltaTicks = 1f;
+            } else {
+                deltaTicks *= deltaTicks;
+            }
+            fovModifier *= 1f - deltaTicks * 0.15f;
+            event.setNewFovModifier(fovModifier);
+        }
     }
 
 }
